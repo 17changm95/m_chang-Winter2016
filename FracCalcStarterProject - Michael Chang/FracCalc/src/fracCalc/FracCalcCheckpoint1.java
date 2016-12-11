56ce7b10e56a3ff8745460dbd125bfec1f1f860f
17changm95 <17changm95@millshighsch.org>
2016-12-11 01:33:50 -0800

    Finished FracCalc

--- FracCalcStarterProject - Michael Chang/FracCalc/src/fracCalc/FracCalcCheckpoint1.java
+++ FracCalcStarterProject - Michael Chang/FracCalc/src/fracCalc/FracCalcCheckpoint1.java
@@ -11,20 +11,21 @@ import java.util.*;
 public class FracCalcCheckpoint1 {
 
     public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
-        String answer = console.nextLine();
+        Scanner console = new Scanner(System.in); //New instance of Scanner object named console
+        String answer = console.nextLine(); //answer will be variable of Scanner object console
         
-        System.out.println(produceAnswer(answer));
+        System.out.println(produceAnswer(answer)); //Outputs second operand in expression, which is input.
     }
 
     public static String produceAnswer(String input) { 
         String[] fracExpr;
         
         if (input.contains(" ")) {
-        	fracExpr = input.split(" ");
-        	return fracExpr[2];
+        	fracExpr = input.split(" "); //Detects for spaces ' ' and creates String array spaces divided by spaces
+        	return fracExpr[2]; //Return last element in String array. Last element is second operand in expression.
         } else {
         	throw new IllegalArgumentException("Please enter spaces between first operand and operator and/or second operand and operator.");
+        	//This throws an error if no spaces are put in between operand and operator.
         }
     }
 }
