17bd052b9d89f35e78f383be776722c304666e64
17changm95 <17changm95@millshighsch.org>
2016-12-12 17:57:16 -0800

    :cookie:

---
+++ DogObject/src/CalculatorLol.java
@@ -0,0 +1,8 @@
+
+public class CalculatorLol {
+	
+	public static void main(String[] args) {
+		System.out.println("Yale Early Admissions Rate: " + 795/0.17);
+		System.out.println(918./6173.);
+	}
+}
